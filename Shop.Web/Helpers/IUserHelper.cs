ce45cc0e5fe1f155d57f61f8247ef262cc84e27b
Juan Zuluaga <carlos.zuluaga@globant.com>
2019-02-28 19:58:48 -0500

    Add font aswerone

--- Shop.Web/Helpers/IUserHelper.cs
+++ Shop.Web/Helpers/IUserHelper.cs
@@ -13,6 +13,13 @@
 
         Task<SignInResult> LoginAsync(LoginViewModel model);
 
+        Task<SignInResult> ValidatePasswordAsync(User user, string password);
+
         Task LogoutAsync();
+
+        Task<IdentityResult> UpdateUserAsync(User user);
+
+        Task<IdentityResult> ChangePasswordAsync(User user, string oldPassword, string newPassword);
+
     }
 }
